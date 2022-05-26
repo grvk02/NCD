30b2e03b5fcb9c2cb5e444347d8b84c9999e1176
Gaurav Kumar <grvk@iitk.ac.in>
2022-05-26 17:24:18 -0400

    Adding Simulation files

---
+++ simulation/system.py
@@ -0,0 +1,15 @@
+class NCD:
+
+	def __init__(self,delta_t,delta_c,t):
+		self.delta_t = delta_t
+		self.delta_c = delta_c
+		self.t = t
+	def rDelta_t(self):
+		return self.delta_t
+	def rDelta_c(self):
+		return self.delta_c	
+	def time(self):
+		return self.t	
+	def trajectory_prop(self)
+	def control(self)
+	def plot_data(self)
