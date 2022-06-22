8111b34fc065c5bf97cabe1eec0c772f34dab01f
Gaurav Kumar <grvk@iitk.ac.in>
2022-06-22 02:14:09 -0400

    changes

--- simulation/system.py
+++ simulation/system.py
@@ -1,39 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy import units as u
+from poliastro.core import elements as elem
 mu = 1; 
+r = [-6045, -3490, 2500] * u.km
+v = [-3.457, 6.618, 2.533] * u.km / u.s
 class NCD:
 
-	def __init__(self,delta_t,delta_c,t,N):
+	def __init__(self,delta_t,delta_c,t,N,U,Lambda_d):
 		self.delta_t = delta_t
 		self.delta_c = delta_c
 		self.t = t
 		self.N = N
-	def rDelta_t(self):
-		return self.delta_t
-	def rDelta_c(self):
-		return self.delta_c	
-	def time(self):
-		return self.t
-	def stage(self):
-		return self.N	
+		self.U = U
+		self.Lambda_d = Lambda_d
+
 	def w(self, delta):
-		return 1 + delta[1]*np.cos(delta[5]) + delta[2]*np.cos(delta[5])
+		return (1 + delta[1]*np.cos(delta[5]) + delta[2]*np.cos(delta[5]))
 	def s2(self,delta):
 		return 1 + delta[3]**2 + delta[4]**2	
-	def rho (self, delta):
-		np.array([0,0,0,0,0,np.sqrt(mu*delta[0])*((w(delta)/delta[0])**2)])
-	def A(delta):
-		return np.sqrt(delta[0]/mu)*np.array([0, 2*delta[0]/w(delta),0],[np.sin(delta[5]),(1/w(delta))*((w(delta)+1)*np.cos(delta[5])+delta[1]), -(delta[2]/w(delta))*(delta[3]*np.sin(delta[5])-delta[4]*np.cos(delta[5]))],[-np.cos(delta[5],(1/w(delta))*((w(delta)+1)*np.sin(delta[5])+delta[2]), (delta[3]/w(delta))*(delta[3]*np.sin(delta[5])-delta[4]*np.cos(delta[5])))],[0,0, (s2(delta)/(2*w(delta))*np.cos(delta[5]))],[0,0,(s2(delta)/(2*w(delta))*np.sin(delta[5])) ],[0,0, (1/w(delta))*(delta[3]*np.sin(delta[5])-delta[4]*np.cos(delta[5]))])
-	def dxi(self, delta_t, delta_c, U, Lambda_d):
-		rho(delta_t)-rho(delta_c)
-		
-		return np.
-				
+	def rho(self, delta):
+		return np.array([0,0,0,0,0,np.sqrt(mu*delta[0])*((self.w(delta)/delta[0])**2)])
+	def A(self,delta):
+		return np.sqrt(delta[0]/mu)*np.array([0, 2*delta[0]/self.w(delta),0],[np.sin(delta[5]),(1/self.w(delta))*((self.w(delta)+1)*np.cos(delta[5])+delta[1]), -(delta[2]/self.w(delta))*(delta[3]*np.sin(delta[5])-delta[4]*np.cos(delta[5]))],[-np.cos(delta[5],(1/self.w(delta))*((self.w(delta)+1)*np.sin(delta[5])+delta[2]), (delta[3]/self.w(delta))*(delta[3]*np.sin(delta[5])-delta[4]*np.cos(delta[5])))],[0,0, (self.s2(delta)/(2*self.w(delta))*np.cos(delta[5]))],[0,0,(self.s2(delta)/(2*self.w(delta))*np.sin(delta[5])) ],[0,0, (1/self.w(delta))*(delta[3]*np.sin(delta[5])-delta[4]*np.cos(delta[5]))])
+	def dxi(self):
+		rho_s = self.rho(self.delta_t)-self.rho(self.delta_c)
+		return rho_s + np.dot(self.A(self.delta_t),self.U) + self.Lambda_d
 	def trajectory_prop(self):
 		dt = 0.1
-		xi = delta_t-delta_c
-	
-	def control(self)
-	def votage_calculation(self)
-	def plot_data(self)
+		xi = self.delta_t-self.delta_c
+	def control(self):
+		pass
+	def votage_calculation(self):
+		pass
+
+	def plot_data(self):
+		pass
 
